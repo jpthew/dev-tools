c61caa4f39a4af19dde22017f732ab4788958587
_x4a_x50 <101289790+jpthew@users.noreply.github.com>
2023-10-31 23:31:39 -0500

    Added check mode

--- whitelistranger.py
+++ whitelistranger.py
@@ -20,10 +20,49 @@ def remove_blacklisted_ips(whitelist, blacklist):
             whitelist.remove(ip)
     return whitelist
 
-# help menu
 def print_help():
-    print("\nThis tool creates a list of IPs from whitelist and blacklist files, creating a clean target list. It handles CIDR notation in the input files.\nIt can also verify that a whitelist file does not include IPs from a chosen blacklist file with --verify\n\nUsage: whitelistranger.py -w whitelist.txt -b blacklist.txt -o output.txt\n\n Flags:\n\n -h: This help menu\n -w: Whitelist file\n -b: Blacklist file\n -o: Output file\n -v: Verbose (print to stdout)\n")
+    print("\nThis tool creates a list of IPs from whitelist and blacklist files, creating a clean target list. It handles CIDR notation in the input files.\nIt can also verify that a whitelist file does not include IPs from a chosen blacklist file with --verify\n\nUsage: whitelistranger.py -w whitelist.txt -b blacklist.txt -o output.txt\n\n Flags:\n\n check: Use check mode to verify whitelist file. (whitelistranger.py check -h) for more information\n -h: This help menu\n -w: Whitelist file\n -b: Blacklist file\n -o: Output file\n -v: Verbose (print to stdout)\n")
 
+def print_check_help():
+    print("Check a whitelist file against a blacklist file to see if it contains any blacklisted IPs. It handles CIDR notation in the input files.\n\nUsage: whitelistranger.py check -w whitelist.txt -b blacklist.txt\n\n Flags:\n\n -h: This help menu\n -w: Whitelist file\n -b: Blacklist file\n -v: Verbose\n")
+
+
+if "check" in sys.argv:
+    if "-h" in sys.argv:
+        print_check_help()
+    else:
+        pass
+    if "-w" in sys.argv:
+        whitelist = read_file(sys.argv[sys.argv.index("-w") + 1])
+    else:
+        print_check_help()
+        sys.exit()
+    if "-b" in sys.argv:
+        blacklist = read_file(sys.argv[sys.argv.index("-b") + 1])
+    else:
+        print_check_help()
+        sys.exit()
+    if "-o" in sys.argv:
+        output_file = sys.argv[sys.argv.index("-o") + 1]
+        bad_ips = []
+        with open(output_file, 'w') as f:
+            for ip in blacklist:
+                if ip in whitelist:
+                    with open(output_file, 'w') as f:
+                        f.write(ip + "\n")
+    if "-v" in sys.argv:
+        print("Checking if whitelist contains blacklisted IPs...")
+        for ip in blacklist:
+            if ip in whitelist:
+                print("Whitelist contains blacklisted IPs")
+                sys.exit()
+        print("Whitelist does not contain blacklisted IPs")
+    else:
+        pass
+    
+    sys.exit()
+
+#  
 
 # get user inputs
 if "-h" in sys.argv:
