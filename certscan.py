23249c2ae83137d74b4ea00a27d192d34160e490
_x4a_x50 <101289790+jpthew@users.noreply.github.com>
2023-11-02 16:01:07 -0500

    added a bunch of QOL with certscan

--- certscan.py
+++ certscan.py
@@ -5,14 +5,21 @@ import ssl
 import socket
 import json
 
-# read file and return certificate information
-def get_cert_info(domain_file):
+def get_cert_info(domain_file): # read file and return certificate information
     cert_info = []
+    try: # check if domain_file exists
+        open(domain_file, "r")
+    except:
+        print("Error: " + domain_file + " does not exist")
+        sys.exit(1)
     with open(domain_file, "r") as f:
         for line in f:
             verbose = False
             if "-v" in sys.argv or "--verbose" in sys.argv:
                 verbose = True
+            output_connect = False
+            if "-o" in sys.argv:
+                output_connect = True
             domain = line.strip()
             ctx = ssl.create_default_context()
             with ctx.wrap_socket(socket.socket(), server_hostname=domain) as s:
@@ -20,22 +27,55 @@ def get_cert_info(domain_file):
                     s.connect((domain, 443))
                     if verbose == True:
                         print("Connected to " + domain)
+                    if output_connect == True:
+                        output_file = sys.argv[sys.argv.index("-o") + 1]
+                        with open(output_file, "a") as f:
+                            f.write("+ " + domain + "\n")
                 except:
                     if verbose == True:
                         print("Failed to connect to " + domain)
+                    if output_connect == True:
+                        output_file = sys.argv[sys.argv.index("-o") + 1]
+                        with open(output_file, "a") as f:
+                            f.write("- " + domain + "\n")
                     continue
                 cert = s.getpeercert()
                 cert_info.append(cert)
     return cert_info
 
+def helppage(): # help page
+    print(
+    """Usage: python3 certscan.py <domain_file>
+
+This script scans a list of domains for SSL/TLS certificates and outputs the information depending on flags set.
+It is an alternative to more louder tools such as nmap and sslscan.
+
+Flags:
+    -o <output_file>    Output domain connection status to file instead of stdout 
+                        --  "+" for success, "-" for failure
+    -oC <output_file>   Output raw certs to file instead of stdout
+    -t <float>          Set custom timeout in seconds for socket connections (Default 2 seconds) 
+                        --  Usage: 
+                                -t 5 (for 5 seconds)
+                                -t 0.5 (for 0.5 seconds)
+    -v, --verbose       Verbose output
+    -h, --help          Display this help message""")
+
 if "-h" in sys.argv or "--help" in sys.argv:
-    print("Usage: python3 certscan.py <domain_file>\n\nFlags:\n    -o <output_file>    Output to file instead of stdout\n   -v, --verbose       Verbose output\n    -h, --help          Display this help message")
+    helppage()
     sys.exit(1)
-if "-o" in sys.argv: # output to file
-    output_file = sys.argv[sys.argv.index("-o") + 1]
+if "-t" in sys.argv: # set timeout
+    socket_timeout = float(sys.argv[sys.argv.index("-t") + 1])
+    socket.setdefaulttimeout(socket_timeout)
+else:
+    socket.setdefaulttimeout(2.0)
+if "-oC" in sys.argv: # output raw certs to file
+    output_file = sys.argv[sys.argv.index("-oC") + 1]
     with open(output_file, "w") as f:
         for cert in get_cert_info(sys.argv[1]):
             f.write(json.dumps(cert) + "\n")
-else:
-    for cert in get_cert_info(sys.argv[1]):
-        print(json.dumps(cert))
+else: 
+    if "-v" in sys.argv:
+        for cert in get_cert_info(sys.argv[1]):
+            print("\n" + json.dumps(cert))
+    else: get_cert_info(sys.argv[1])
